4ee7633a5e11fc739dc2e7b65f3962dc49439e61
ximet <dq.ximet@gmail.com>
2017-10-20 19:45:26 +0300

    add first checker

---
+++ index.js
@@ -0,0 +1,8 @@
+
+const checkAltAttributeForImg = (context, logErrorCallback) => {
+    for (const img of context.querySelectorAll('img')) {
+        if (!img.hasAttribute('alt')) {
+            logErrorCallback(new ImageWithoutAltAttributeError(img))
+        }
+    }
+}
