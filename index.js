0c40df0f21d4e451b21eca6a0b43a97d8aa5dc92
ximet <dq.ximet@gmail.com>
2017-10-21 13:53:51 +0300

    add 2 helpers

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 import { ImageWithoutAltAttributeError } from './Error.js';
 
+const isHidden = (element) => {
+    return element.closest('[aria-hidden="true"], [hidden], [style*="display: none"]') != null
+}
+  
+const isText = (value) =>{
+    return toString.call(value) === '[object String]';
+}
+
 const checkAltAttributeForImg = (context, logErrorCallback) => {
     for (const img of context.querySelectorAll('img')) {
         if (!img.hasAttribute('alt')) {
